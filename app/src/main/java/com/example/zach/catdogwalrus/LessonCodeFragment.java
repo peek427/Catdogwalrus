e89e6ccf13ed8e3391b687216e1954828c99bf89
Andrew Kusel <ark10e@my.fsu.edu>
2015-01-29 05:44:06 -0500

    Added the basic layout for the lessons.  Added class Lesson1_1, which contains 2 fragments, one for the text of the lesson and one for the source code (LessonTextFragment and LessonCodeFragment).  Didn't add any content to the lesson yet.

---
+++ app/src/main/java/com/example/zach/catdogwalrus/LessonCodeFragment.java
@@ -0,0 +1,18 @@
+package com.example.zach.catdogwalrus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+
+public class LessonCodeFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.lesson_code, container, false);
+    }
+}
