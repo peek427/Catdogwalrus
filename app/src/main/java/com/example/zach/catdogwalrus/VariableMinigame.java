bae15ce3906bdb811ed66861691a47ee766e6c80
peek427 <peek427@gmail.com>
2015-02-11 23:27:37 -0500

    Changes to first minigame, added a test button in the main menu for easy access

--- app/src/main/java/com/example/zach/catdogwalrus/VariableMinigame.java
+++ app/src/main/java/com/example/zach/catdogwalrus/VariableMinigame.java
@@ -1,14 +1,22 @@
 package com.example.zach.catdogwalrus;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ClipData;
+import android.content.Context;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.DragEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnDragListener;
@@ -18,31 +26,42 @@ import android.content.Intent;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import android.os.Handler;
 
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Zach on 1/27/2015.
  */
 public class VariableMinigame extends Minigame {
 
+    long starttime;
+
+    final Context context = this; // Used to refer to context within listeners
+    Toast toasty; // TOAST USED FOR SCORING
+
     private TextView thevariable; // The variable to be dragged into the correct boxes by the user
 
     private TextView thescore; // Textview for the score
     private TextView thetimer; // Textview for the timer
 
+    private TextView dialogscore;
+    private TextView dialoghighscore;
+
     private ImageView charbox, intbox, boolbox, stringbox, doublebox; // The imageviews representing the boxes to be dragged onto
 
     private StringBucket thebucket; // The bucket that holds the strings to be loaded into the draggable variable
 
+    private Handler myhandler = new Handler(); // Handler for the timer
 
-    private class StringBucket {
+    private class StringBucket {  // Container that holds the strings to be used in variable minigame.
         private Map<Integer, String> stringmap = new HashMap<Integer, String>();
         protected int CurrentString; // holds the key of the current string
 
 
-        public StringBucket() // Container that holds the strings to be used in variable minigame.
+        public StringBucket()
         {
 
 
@@ -66,7 +85,7 @@ public class VariableMinigame extends Minigame {
             stringmap.put(12, "'@'");
             stringmap.put(13, "'x'");
             stringmap.put(14, "'^'");
-            stringmap.put(15, "'\"'");
+            stringmap.put(15, "'$'");
             stringmap.put(16, "'y'");
             stringmap.put(17, "'k'");
             stringmap.put(18, "'.'");
@@ -122,6 +141,7 @@ public class VariableMinigame extends Minigame {
         setContentView(R.layout.variable_minigame);
 
 
+
         thevariable = (TextView) findViewById(R.id.VariableGameString); // Connect the "variable" textview to the layout
         thevariable.setOnTouchListener(new VariableListener()); // The listener used to detect touch on the "variable" textview
         thescore = (TextView) findViewById(R.id.Score);
@@ -141,33 +161,60 @@ public class VariableMinigame extends Minigame {
 
         // initialize stringbucket
         thebucket = new StringBucket();
+        thebucket.generaterandom();
+        thevariable.setText(thebucket.stringmap.get(thebucket.CurrentString)); // set the first variable to the first random generation
 
         //initialize score
         Score = 0;
         thescore.setText("0");
 
+
         //initialize timer
         thetimer = (TextView) findViewById(R.id.VariableGameTimer);
 
-        new CountDownTimer(30000, 1000) {               // THIS LINE STARTS THE TIMER
-            public void onTick(long millisecs) { // CALLED EVERY TICK
-                thetimer.setText("" + (millisecs / 1000)); // Output the number of seconds left
 
-            }
+        // THIS IS WHERE THE TIMER STARTS
+        starttime = SystemClock.uptimeMillis();
+        myhandler.postDelayed(TimerThread, 0);
 
-            public void onFinish() { // CALLED WHEN THE TIMER IS FINISHED, SO LOCK THE GAME & OUTPUT RESULTS
-                thetimer.setText("0");
+
+
+    } // end of onCreate
+
+    private Runnable TimerThread = new Runnable()
+    {
+        long ms = 0L;
+        long buffer = 0L;
+        long updated = 0L;
+        public void run() {
+
+            ms = SystemClock.uptimeMillis() - starttime;
+            updated = buffer + ms;
+
+            int sec = (int) (updated / 1000);
+            thetimer.setText("" + (30 - sec));
+            if (sec < 30)
+                myhandler.postDelayed(this, 0);
+            else {
+
+
+               //IMPORTANT ********************************************************************************
+                // THIS CODE SEGMENT IS HOW YOU MAKE THE END-OF-GAME SCORE DIALOG BOX APPEAR
+                FragmentManager fm = getSupportFragmentManager();
+                Score_alert_fragment alertfragment = new Score_alert_fragment();
+                alertfragment.y=Score; // value of score
+                alertfragment.h=0; // value of high score
+                alertfragment.show(fm, "scorefrag");
+                // END OF CODE SEGMENT ******************************************************************
+
 
                 // Ideally, a sound will be played, and a window will pop up on top of the variable
                 // TextView that shows what score you reached and your old highscore. It can also
                 // display whether or not the next lesson has been unlocked already by your score.
 
-
             }
-
-        }; // end of timer description
-    } // end of onCreate
-
+        }
+    };
 
     private class VariableListener implements OnTouchListener {
         // Listener that handles the variable being touched
@@ -175,6 +222,7 @@ public class VariableMinigame extends Minigame {
         public boolean onTouch(View v, MotionEvent m) {
             if (m.getAction() == MotionEvent.ACTION_DOWN) // "A pressed gesture has started" on the variable
             {
+                thevariable.setVisibility(View.INVISIBLE);
                 View.DragShadowBuilder shadow = new View.DragShadowBuilder(v); // Create a dragshadowbuilder for the variable
                 ClipData clipped = ClipData.newPlainText(".", "."); // Empty clipdata, may be used later
                 v.startDrag(clipped, shadow, v, 0); // This marks the start of a drag
@@ -194,7 +242,7 @@ public class VariableMinigame extends Minigame {
         {
             ImageView box = (ImageView) v;
             TextView var = (TextView) d.getLocalState(); // var holds the actual textview
-            if (d.getAction() == DragEvent.ACTION_DROP) // This is the only case we are concerned about
+            if (d.getAction() == DragEvent.ACTION_DROP)
             {
                 if (box.getId() == R.id.Box1) // if the box dropped onto is the BOOL box
                 {
@@ -282,6 +330,16 @@ public class VariableMinigame extends Minigame {
 
 
             } // end of Action.Event_drop
+
+            if (d.getAction() == DragEvent.ACTION_DRAG_ENDED)
+            {
+                final View dropped = (View) d.getLocalState();
+                dropped.post(new Runnable(){
+                    public void run(){
+                    dropped.setVisibility(View.VISIBLE);}
+                });
+            }
+
             return true;
         } // end of onDrag
 
