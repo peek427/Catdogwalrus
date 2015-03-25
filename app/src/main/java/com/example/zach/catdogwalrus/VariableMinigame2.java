0a2bca52c00ea999478e59621df2d134f6e5e2c0
Madeline Richards <mar11t@my.fsu.edu>
2015-03-25 17:16:07 -0400

    added my string to strings.

---
+++ app/src/main/java/com/example/zach/catdogwalrus/VariableMinigame2.java
@@ -0,0 +1,40 @@
+package com.example.zach.catdogwalrus;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.view.View.DragShadowBuilder;
+import android.content.Intent;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import android.os.Handler;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Madeline on 3/23/2015.
+ */
+public class VariableMinigame2 {
+}
